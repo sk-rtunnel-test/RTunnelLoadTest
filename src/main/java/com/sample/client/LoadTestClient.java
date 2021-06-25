f90d52b441496e16014b11e50edb35845089b686
skhanolkar <skhanolkar@objective.com>
2021-06-25 14:52:17 +1000

    First version of the Load Tester.

---
+++ src/main/java/com/sample/client/LoadTestClient.java
@@ -0,0 +1,56 @@
+package com.sample.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Date;
+
+public class LoadTestClient extends Thread {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoadTestClient.class);
+
+    private static final int NUM_PARALLEL_THREADS = 1000;
+    private static final int REPORT_EACH_THREAD_AFTER_NUM_MESSAGES = 10;
+
+    private static final String RTUNNEL_SERVER_HOST = "172.16.20.183";
+    private static final int RTUNNEL_SERVER_FORWARD_PORT = 9004;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < NUM_PARALLEL_THREADS; i++) {
+            new LoadTestClient().start();
+        }
+    }
+
+    public void run() {
+
+        RestTemplate restTemplate = new RestTemplate();
+        String pingResourceUrl = "http://" + RTUNNEL_SERVER_HOST + ":" + RTUNNEL_SERVER_FORWARD_PORT + "/ping";
+
+        int total_messages = 0;
+        int total_rtunnel_successful_messages = 0;
+
+        while (true) {
+            try {
+                total_messages++;
+
+                HttpStatus response = restTemplate.getForEntity(pingResourceUrl, String.class).getStatusCode();
+
+                if (response.equals(HttpStatus.OK)) {
+                    total_rtunnel_successful_messages++;
+                } else {
+                    logger.info("There was an Rtunnel failure message");
+                    logger.info(response.toString());
+                }
+                if (total_messages % REPORT_EACH_THREAD_AFTER_NUM_MESSAGES == 0) {
+                    logger.info(new Date().toString());
+                    logger.info("Total Messages = " + total_messages);
+                    logger.info("Total Rtunnel successful messages = " + total_rtunnel_successful_messages);
+                }
+            } catch (Exception ex) {
+                logger.error("Error Processing Packet.", ex);
+            }
+        }
+    }
+}
---
+++ src/main/java/com/sample/server/PingController.java
@@ -0,0 +1,13 @@
+package com.sample.server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "{\"result\": \"ping successful\"}";
+    }
+}
